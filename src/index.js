a90fcb0ee0f3aa31887037432ebe8bded0572731
Proldov <vadim54.00@mail.ru>
2020-09-30 03:14:21 +0300

    the solution of the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function toReadable (number) {
+	let units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+	let iNum = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+	let dozen = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+	let result = [];
+	if(number >= 100) {
+		result.push(units[parseInt(number / 100)] + ' hundred')
+		if(number % 100 > 0) {
+			number = number % 100
+		} else {
+			number
+		}
+	}
+
+	if(number >= 20) {
+		result.push(dozen[parseInt(number/10)]);
+		result.push(units[parseInt(number % 10)])
+	} else {
+			
+		if(number >= 10) {
+			result.push(iNum[parseInt(number % 10)])
+		} else {
+			if(number >= 1) {
+				result.push(units[number])
+				} else {
+					result.push(['zero'])
+				}
+		}
+		
+		
+	}
+
+	return result.join(' ').trim()
   
 }
